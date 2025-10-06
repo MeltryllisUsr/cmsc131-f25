ec0102a119ce0cb5df8384ee4ed888eba8e96194
Ethan Neal <ethanisaiahneal@gmail.com>
2025-10-06 19:32:43 -0400

    Your commit message here

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private String accountId;
+    private double balance;
+    private String accountName;
+    private String accountType;
+
+    public String getAccountId() {
+        return accountId;
+    }
+    
+    public String getAccountName() {
+        return accountName;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+
+    
+}
---
+++ src/Bank.java
@@ -0,0 +1,34 @@
+public class Bank {
+    private Account[] accounts;
+    private int count;
+
+    public class Bank() {
+        accounts = new Account[100];
+        count = 0;
+    }
+
+    public void addAccount(Account acc) {
+        accounts[count]=acc;
+        count++;
+    }
+
+    public Account getAccount(String accountID) {
+        for (int i = 0; i <count; i++){
+            if accounts[i].getAccountId().equals(accoundID){
+            return accounts[i];
+                }
+            }
+            return null;
+        }
+    public int getCount{
+        return count;
+        }
+    public Account getAccountName{
+        for (int i=0; i<count; i++){
+            if accounts[i].getAccountName().equals(accountName){
+                return accounts[i];
+                }
+            }
+        }
+    }
+}
